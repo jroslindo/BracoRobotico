d839bb69500966e219711a11b1bec6f7006e616e
jroslindo <jroslindotab@gmail.com>
2019-05-03 07:26:44 -0300

    implementando funcao unica para ombro

--- tentativa1/scripts/scriptjava.js
+++ tentativa1/scripts/scriptjava.js
@@ -1,9 +1,15 @@
 var grausAresta = -45;
 
-function teste (){
-    grausAresta+=5;
+function teste (direita){
+    if (direita == 0)
+        grausAresta-=5;
+
+    if (direita == 1){
+        grausAresta+=5;
+    }   
+    
     document.getElementById('aresta').style.WebkitTransform = 'rotate(' + grausAresta.toString() +'deg)';
   
-    console.log(grausAresta.toString())
+    console.log(direita.toString())
 }
 
